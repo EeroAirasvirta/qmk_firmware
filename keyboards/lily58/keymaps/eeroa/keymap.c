4bb0c3813652d4d3a8589457dc1b07041f855521
Eero Airasvirta <13832245+EeroAirasvirta@users.noreply.github.com>
2023-02-09 20:28:00 +0200

    Cleaning

--- keyboards/lily58/keymaps/eeroa/keymap.c
+++ keyboards/lily58/keymaps/eeroa/keymap.c
@@ -109,11 +109,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                    |      |      |      |QWERTY|COLMAK|      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|      |      |RGB ON| HUE+ | SAT+ | VAL+ |
+ * |      |      |      |      |      |      |-------.    ,-------|      |      |      |      |      |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
+ * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP|      |
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -142,24 +142,10 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 // When you add source files to SRC in rules.mk, you can use functions.
 const char* read_layer_state(void);
 const char* read_logo(void);
-//void set_keylog(uint16_t keycode, keyrecord_t *record);
-//const char *read_keylog(void);
-//const char *read_keylogs(void);
-
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
 
 bool oled_task_user(void) {
   if (is_keyboard_master()) {
-    // If you want to change the display of OLED, you need to change here
     oled_write_ln(read_layer_state(), false);
-    //oled_write_ln(read_keylog(), false);
-    //oled_write_ln(read_keylogs(), false);
-    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-    //oled_write_ln(read_host_led_state(), false);
-    //oled_write_ln(read_timelog(), false);
   } else {
     oled_write(read_logo(), false);
   }
@@ -168,22 +154,16 @@ bool oled_task_user(void) {
 #endif // OLED_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-//   if (record->event.pressed) {
-// #ifdef OLED_ENABLE
-    // set_keylog(keycode, record);
-// #endif
-   // set_timelog();
-//   }
   switch (keycode)
   {
   case QWERTY:
     if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
+      set_single_persistent_default_layer(_QWERTY);
     }
     break;
   case COLEMAK:
     if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
+      set_single_persistent_default_layer(_COLEMAK);
     }
     break;
   default:
