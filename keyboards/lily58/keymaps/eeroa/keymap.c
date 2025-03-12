345223d0b055a99babdfffbc3c4b261156634bf9
Eero Airasvirta <eero.airasvirta@luckyfew.fi>
2025-03-12 17:26:57 +0200

    Add layer for mac with left opt&cmd swapped

--- keyboards/lily58/keymaps/eeroa/keymap.c
+++ keyboards/lily58/keymaps/eeroa/keymap.c
@@ -5,6 +5,7 @@
 enum layer_number {
   _QWERTY = 0,
   _QW_COMP,
+  _QW_COMP_MAC,
   _COLEMAK,
   _LOWER,
   _RAISE,
@@ -14,6 +15,7 @@ enum layer_number {
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   QW_COMP,
+  QW_COMP_MAC,
   COLEMAK,
 };
 
@@ -62,6 +64,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L, FI_ODIA, FI_ADIA,
   KC_LSFT, FI_LABK, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   FI_DIAE,  KC_N,    KC_M, FI_COMM,  FI_DOT, FI_MINS, KC_RSFT,
                         KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,    KC_ENT, MO(_RAISE), KC_BSPC, KC_RALT
+),
+
+/* QWERTY (Compatibility layout for custom software layout) + Alt and Cmd swapped.
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  +\? |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |   '  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |LCtrl |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   Ö  |   Ä  |
+ * |------+------+------+------+------+------|   B   |    |  ¨^~  |------+------+------+------+------+------|
+ * |LShift|  <>| |   Z  |   X  |   C  |   V  |-------|    |-------|   N  |   M  |   ,  |   .  |   -  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RAlt |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+
+ [_QW_COMP_MAC] = LAYOUT(
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0, FI_PLUS,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, FI_QUOT,
+  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L, FI_ODIA, FI_ADIA,
+  KC_LSFT, FI_LABK, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   FI_DIAE,  KC_N,    KC_M, FI_COMM,  FI_DOT, FI_MINS, KC_RSFT,
+                        KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC,    KC_ENT, MO(_RAISE), KC_BSPC, KC_RALT
 ),
 
 /* COLEMAK (Modified)
@@ -133,9 +158,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      |      |QWERTY|Q_COMP|COLMAK|      | // Persistent
+ * |      |      |      |      |      |      |                    |      |QWERTY|Q_CO_M|Q_COMP|COLMAK|      | // Persistent
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|      |      |QWERTY|Q_COMP|COLMAK|      | // Temporary
+ * |      |      |      |      |      |      |-------.    ,-------|      |QWERTY|Q_CO_M|Q_COMP|COLMAK|      | // Temporary
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -145,8 +170,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   [_ADJUST] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX,      XXXXXXX,      XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, QWERTY,      QW_COMP,      COLEMAK,      XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, DF(_QWERTY), DF(_QW_COMP), DF(_COLEMAK), XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, QWERTY,  QW_COMP_MAC, QW_COMP,      COLEMAK,      XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, DF(_QWERTY), DF(_QW_COMP_MAC), DF(_QW_COMP), DF(_COLEMAK), XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX,      XXXXXXX,      XXXXXXX,
                              _______, _______, _______, _______, _______,  _______, _______, _______
   )
@@ -190,6 +215,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   case QW_COMP:
     if (record->event.pressed) {
       set_single_persistent_default_layer(_QW_COMP);
+    }
+    break;
+  case QW_COMP_MAC:
+    if (record->event.pressed) {
+      set_single_persistent_default_layer(_QW_COMP_MAC);
     }
     break;
   case COLEMAK:
